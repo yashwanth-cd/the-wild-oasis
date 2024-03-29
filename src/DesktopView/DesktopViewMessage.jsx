dd637e44964291f8359f4abc051f81e0961db52d
yashwanth-cd <yashwanth.3270@gmail.com>
2024-03-29 22:13:09 +0530

    44th commit - Fixed the desktop message and icon

--- src/DesktopView/DesktopViewMessage.jsx
+++ src/DesktopView/DesktopViewMessage.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import styled from "styled-components";
 import Heading from "../ui/Heading";
-import { CgDanger } from "react-icons/cg";
+import { FaLaptopCode } from "react-icons/fa";
 import { useEffect, useState } from "react";
 
 const StyledMessage = styled.div`
@@ -10,7 +10,7 @@ const StyledMessage = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
-  background-color: var(--color-grey-400);
+  background-color: var(--color-grey-50);
   overflow: hidden;
 
   h1 {
@@ -37,15 +37,24 @@ export default function DesktopViewMessage({ children }) {
 
   useEffect(function () {}, [desktopWidth]);
 
+  if (desktopWidth < 640) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+
   if (desktopWidth >= 1400) {
     return children;
   }
 
   return (
     <StyledMessage>
-      <Heading as="h1">
-        <CgDanger style={{ width: "35px", marginRight: "10px" }} /> Please
-        switch to desktop view for better experience
+      <Heading
+        as="h1"
+        style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+      >
+        <FaLaptopCode style={{ color: "#292929", fontSize: "125px" }} />
+        Please switch to a desktop for better experience
       </Heading>
     </StyledMessage>
   );
